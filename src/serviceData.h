772ebe98b8b313775afea3b0800802a19c6e2a3c
Saveliy Savenak <56991906+Lisoveliy@users.noreply.github.com>
2023-11-04 21:20:31 +0300

    Update serviceData.h

--- src/serviceData.h
+++ src/serviceData.h
@@ -4,12 +4,12 @@
 namespace ServiceData
 {
 	const String SSID = "IoT-1986";
-	const String Password = "IoTPassword228";
+	const String Password = "12345678";
 	namespace Mqtt{
 		const String Server = "homeassistant.local";
 		const short Port = 1883;
 		const String User = "esp8266dev";
-		const String Password = "iotpassword228"; 
+		const String Password = "12345678"; 
 		const String Branch = "esp8266light/state";
 	}
 	namespace Commands
