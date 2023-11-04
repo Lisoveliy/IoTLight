55bd83a0de8d5520697807338912ad16248d7445
Lisoveliy (VS Code) <1986developer@gmail.com>
2023-11-04 14:30:06 +0300

    Added namespaces

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,40 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <serverControl.h>
 
-const String SSID = "IoT-1986";
-const String Password = "IoTPassword228";
+namespace ServiceData
+{
+	const String SSID = "IoT-1986";
+	const String Password = "IoTPassword228";
+	namespace Mqtt{
+		const String Server = "mosquitto";
+		const short Port = 1883;
+		const String User = "esp8266dev";
+		const String Password = "iotpassword228"; 
+	}
+};
 
-ServerControl* control;
+ServerControl *control;
 void setup()
 {
 	pinMode(LED_BUILTIN, OUTPUT);
 	pinMode(D0, OUTPUT);
 	digitalWrite(D0, LOW);
 	digitalWrite(LED_BUILTIN, LOW);
-	WiFi.begin(SSID, Password);
+	WiFi.begin(ServiceData::SSID, ServiceData::Password);
 	Serial.begin(115200);
-	while (WiFi.status() != WL_CONNECTED )
+	while (WiFi.status() != WL_CONNECTED)
 	{
 		Serial.println(WiFi.status());
 		delay(100);
 	}
 	Serial.println("Connected");
 	digitalWrite(LED_BUILTIN, HIGH);
-	control = new ServerControl();;
+	control = new ServerControl();
+	;
 }
-void loop(){
+void loop()
+{
 	control->handleClient();
 }
