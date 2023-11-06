e91f89ec6705eb90d07130a645a1ccec6a5aede2
Lisoveliy (VS Code) <1986developer@gmail.com>
2023-11-06 13:56:55 +0300

    Created secrets

--- src/serviceData.h
+++ src/serviceData.h
@@ -1,15 +1,17 @@
 #include <Arduino.h>
+#include <defines.h>
+//Use defines.h.example to create your config
 #pragma once
 
 namespace ServiceData
 {
-	const String SSID = "IoT-1986";
-	const String Password = "12345678";
+	const String SSID = WiFiSSID;
+	const String Password = WiFiPass;
 	namespace Mqtt{
-		const String Server = "homeassistant.local";
+		const String Server = ServerIP;
 		const short Port = 1883;
 		const String User = "esp8266dev";
-		const String Password = "12345678"; 
+		const String Password = MQTTPass; 
 		const String Branch = "esp8266light/state";
 	}
 	namespace Commands
