faf57be832d6fed1714c097a599cb8d217a135d7
Lisoveliy (VS Code) <1986developer@gmail.com>
2023-11-10 17:58:53 +0300

    Run soft AP if can't connect

--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,50 @@
 
 ServerControl *control;
 
-void connectToAP()
+void setupWiFi()
 {
+	Serial.println("Running AP...");
+	ESP8266WebServer server(80);
+
+	WiFi.softAP(ServiceData::ApSSID, ServiceData::ApPassword);
+	WiFi.softAPConfig(ServiceData::local_ip, ServiceData::gateway, ServiceData::subnet);
+}
+int connectToAP()
+{
+	int counter = 0;
 	digitalWrite(LED_BUILTIN, LOW);
 	while (WiFi.status() != WL_CONNECTED)
 	{
+		if (counter > 100)
+		{
+			return 1;
+		}
 		Serial.println(WiFi.status());
 		delay(100);
+		counter++;
 	}
 
 	Serial.print("Connected to ");
 	Serial.println(ServiceData::SSID);
 	digitalWrite(LED_BUILTIN, HIGH);
+	return 0;
 }
 void setupServices()
 {
-	connectToAP();
-	delay(200);
-	digitalWrite(LED_BUILTIN, LOW);
-	delay(200);
-	digitalWrite(LED_BUILTIN, HIGH);
-	delay(200);
-	control = new ServerControl();
+	if (connectToAP() == 0)
+	{
+		delay(200);
+		digitalWrite(LED_BUILTIN, LOW);
+		delay(200);
+		digitalWrite(LED_BUILTIN, HIGH);
+		delay(200);
+		control = new ServerControl();
+	}
+	else
+	{
+		setupWiFi();
+	}
 }
-
 void setup()
 {
 	pinMode(LED_BUILTIN, OUTPUT);
@@ -41,7 +61,8 @@ void setup()
 
 void loop()
 {
-	if(WiFi.status() != WL_CONNECTED){
+	if (WiFi.status() != WL_CONNECTED)
+	{
 		connectToAP();
 	}
 	control->handleClient();
--- src/serviceData.h
+++ src/serviceData.h
@@ -1,17 +1,18 @@
 #include <Arduino.h>
 #include <defines.h>
-//Use defines.h.example to create your config
+// Use defines.h.example to create your config
 #pragma once
 
 namespace ServiceData
 {
 	const String SSID = WiFiSSID;
 	const String Password = WiFiPass;
-	namespace Mqtt{
+	namespace Mqtt
+	{
 		const String Server = ServerIP;
 		const short Port = 1883;
 		const String User = "esp8266dev";
-		const String Password = MQTTPass; 
+		const String Password = MQTTPass;
 		const String Branch = "esp8266light/state";
 	}
 	namespace Commands
@@ -20,4 +21,10 @@ namespace ServiceData
 		const String On = "ON";
 		const String Off = "OFF";
 	}
+	const String ApSSID = "NodeMCU";	// Enter SSID here
+	const String ApPassword = "12345678"; // Enter Password here
+
+	const IPAddress local_ip(192, 168, 1, 1);
+	const IPAddress gateway(192, 168, 1, 1);
+	const IPAddress subnet(255, 255, 255, 0);
 };
